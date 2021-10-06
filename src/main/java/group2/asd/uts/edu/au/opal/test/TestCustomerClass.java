eed2be52cbe6b97debfad1141594b542de3743a4
James Samios <james@samios.io>
2021-10-06 13:52:02 +1100

    Add test cases for customer

--- src/main/java/group2/asd/uts/edu/au/opal/test/TestCustomerClass.java
+++ src/main/java/group2/asd/uts/edu/au/opal/test/TestCustomerClass.java
@@ -1,20 +1,45 @@
 package group2.asd.uts.edu.au.opal.test;
 
+import group2.asd.uts.edu.au.opal.model.Address;
+import group2.asd.uts.edu.au.opal.model.Customer;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class TestCustomerClass {
+
+    private final String firstName = "Barry";
+    private final String lastName = "Johnson";
+    private final String email = "barry.johnson@uts.com";
+    private final String phone = "0412222345";
+    private final String password = "Barry123!";
+    private final Address address = new Address("Apartment 1A", "123 Sydney Street", "Sydney", 2000, "NSW");
+
+    private final Customer expectedCustomer = new Customer(firstName, lastName, email, phone, password, address);
+
     @Test
-    public void testCustomerConstructor() {
-        String str = "Junit test";
-        assertEquals(str, "Junit test");
+    public void testGetCustomerFirstName() {
+        assertEquals(firstName, expectedCustomer.getFirstName());
     }
 
     @Test
-    public void testConvertClassToDocument() {
-        assertEquals("Expected", "Expected");
+    public void testGetCustomerLastName() {
+        assertEquals(lastName, expectedCustomer.getLastName());
     }
 
+    @Test
+    public void testCustomerGetEmail() {
+        assertEquals(email, expectedCustomer.getEmailAddress());
+    }
+
+    @Test
+    public void testCustomerPhone() {
+        assertEquals(phone, expectedCustomer.getPhoneNumber());
+    }
+
+    @Test
+    public void testCustomerAddress() {
+        assertEquals(address, expectedCustomer.getAddress());
+    }
 
 }
